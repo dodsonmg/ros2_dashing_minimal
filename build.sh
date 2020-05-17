c7bae7cdb829f599ed6326f88875d8f2ff4f765d
Michael Dodson <md403@cam.ac.uk>
2020-05-17 13:49:57 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# build command for colcon
+
+TOOLCHAIN_PATH="/home/broomstick/cheri/ros2_dashing_min/CrossToolchain.cmake"
+
+# necessary to find the .cmake files for poco pre-built library
+CMAKE_POCO_PATH="/home/broomstick/cheri/output/sdk/sysroot128/usr/local/mips-purecap/libcheri/cmake"
+
+if [ "$#" -eq 0 ]; then
+	colcon build \
+		--packages-skip-build-finished \
+		--cmake-args \
+			-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH \
+			-DBUILD_TESTING=NO \
+			-DTHIRDPARTY=ON \
+			-DCMAKE_PREFIX_PATH="$CHERI_PREFIX_PATH;$CMAKE_PREFIX_PATH" \
+		--no-warn-unused-cli
+
+elif [ "$#" -eq 1 ]; then
+    if [ "$1" = "clean" ]; then
+		rm -r ./build ./install
+		colcon build \
+			--cmake-args \
+				-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH \
+				-DBUILD_TESTING=NO \
+				-DTHIRDPARTY=ON \
+				-DCMAKE_PREFIX_PATH="$CMAKE_POCO_PATH;$CMAKE_PREFIX_PATH" \
+			--no-warn-unused-cli
+    else
+		rm -r ./build/$1 ./install/$1
+		colcon build \
+			--packages-skip-build-finished \
+			--packages-up-to=$1 \
+			--cmake-args \
+				-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH \
+				-DBUILD_TESTING=NO \
+				-DTHIRDPARTY=ON \
+				-DCMAKE_PREFIX_PATH="$CMAKE_POCO_PATH;$CMAKE_PREFIX_PATH" \
+			--no-warn-unused-cli
+    fi
+elif [ "$#" -eq 2 ]; then
+    if [ "$2" = "clean" ]; then
+		rm -r ./build/$1 ./install/$1
+		colcon build \
+			--packages-up-to=$1 \
+			--cmake-args \
+				-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH \
+				-DBUILD_TESTING=NO \
+				-DTHIRDPARTY=ON \
+				-DCMAKE_PREFIX_PATH="$CMAKE_POCO_PATH;$CMAKE_PREFIX_PATH" \
+			--no-warn-unused-cli
+	else
+	    echo "Usage: ./build.sh [package] [clean]"
+    fi
+else
+    echo "Usage: ./build.sh [package] [clean]"
+fi
---
+++ build_ubuntu.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# build command for colcon
+
+# TOOLCHAIN_PATH="/home/broomstick/cheri/ros2_dashing_min/CrossToolchain_old.cmake"
+
+# necessary to find the .cmake files for poco pre-built library
+# CHERI_PREFIX_PATH="/home/broomstick/cheri/output/sdk/sysroot128/usr/local/mips-purecap/libcheri/cmake"
+
+if [ "$#" -eq 0 ]; then
+	colcon build \
+		--packages-skip-build-finished \
+		--build-base ./build_ubuntu \
+		--install-base ./install_ubuntu \
+		--cmake-args \
+			-DBUILD_TESTING=NO \
+			-DTHIRDPARTY=ON \
+		--no-warn-unused-cli
+
+elif [ "$#" -eq 1 ]; then
+    if [ "$1" = "clean" ]; then
+		rm -r ./build_ubuntu ./install_ubuntu
+		colcon build \
+			--build-base ./build_ubuntu \
+			--install-base ./install_ubuntu \
+			--cmake-args \
+				-DBUILD_TESTING=NO \
+				-DTHIRDPARTY=ON \
+			--no-warn-unused-cli
+    else
+		rm -r ./build_ubuntu/$1 ./install_ubuntu/$1
+		colcon build \
+			--packages-skip-build-finished \
+			--build-base ./build_ubuntu \
+			--install-base ./install_ubuntu \
+			--packages-up-to=$1 \
+			--cmake-args \
+				-DBUILD_TESTING=NO \
+				-DTHIRDPARTY=ON \
+			--no-warn-unused-cli
+    fi
+elif [ "$#" -eq 2 ]; then
+    if [ "$2" = "clean" ]; then
+		rm -r ./build_ubuntu/$1 ./install_ubuntu/$1
+		colcon build \
+			--packages-up-to=$1 \
+			--build-base ./build_ubuntu \
+			--install-base ./install_ubuntu \
+			--cmake-args \
+				-DBUILD_TESTING=NO \
+				-DTHIRDPARTY=ON \
+			--no-warn-unused-cli
+	else
+	    echo "Usage: ./build.sh [package] [clean]"
+    fi
+else
+    echo "Usage: ./build.sh [package] [clean]"
+fi
