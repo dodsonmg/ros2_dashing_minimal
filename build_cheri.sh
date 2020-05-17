f37bcc2655bf66291c33cd261d240eb57d340f89
Michael Dodson <md403@cam.ac.uk>
2020-05-17 13:58:12 +0100

    name changes

--- build_cheri.sh
+++ build_cheri.sh
@@ -2,10 +2,10 @@
 #
 # build command for colcon
 
-TOOLCHAIN_PATH="/home/broomstick/cheri/ros2_dashing_min/CrossToolchain.cmake"
+TOOLCHAIN_PATH="$HOME/cheri/ros2_dashing_min/CrossToolchain.cmake"
 
 # necessary to find the .cmake files for poco pre-built library
-CMAKE_POCO_PATH="/home/broomstick/cheri/output/sdk/sysroot128/usr/local/mips-purecap/libcheri/cmake"
+CMAKE_POCO_PATH="$HOME/cheri/output/sdk/sysroot128/usr/local/mips-purecap/libcheri/cmake"
 
 if [ "$#" -eq 0 ]; then
 	colcon build \
@@ -14,7 +14,7 @@ if [ "$#" -eq 0 ]; then
 			-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH \
 			-DBUILD_TESTING=NO \
 			-DTHIRDPARTY=ON \
-			-DCMAKE_PREFIX_PATH="$CHERI_PREFIX_PATH;$CMAKE_PREFIX_PATH" \
+			-DCMAKE_PREFIX_PATH="$CMAKE_POCO_PATH;$CMAKE_PREFIX_PATH" \
 		--no-warn-unused-cli
 
 elif [ "$#" -eq 1 ]; then
