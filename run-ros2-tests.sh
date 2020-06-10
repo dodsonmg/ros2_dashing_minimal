3ab64cf3fc15b3de9a9c715a4f1b5944cc2df134
Michael Dodson <md403@cam.ac.uk>
2020-06-10 11:25:17 +0100

    clean up cheribuild test script

--- run-ros2-tests.sh
+++ run-ros2-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -xe
 
+# test run via cheribuild:
+# ./cheribuild.py ros2-mips-purecap --test (or similar)
+
 ROS2_ROOT="$(realpath .)"
 if ! test -e "${ROS2_ROOT}/run-ros2-tests.sh"; then
    echo "You have to cd to the directory where $0 is located first!"
@@ -8,17 +11,8 @@ fi
 
 ROS2_TEST="${ROS2_ROOT}/install/ros2_cheribsd_test/lib/ros2_cheribsd_test/cheribsd_test"
 
-echo "${0}: ROS2 test binary details:"
-if ! command -v file > /dev/null; then
-	echo "file binary not installed"
-else
-	file "${ROS2_TEST}"
-fi
-
 # set LD_CHERI_LIBRARY_PATH and LD_LIBRARY_PATH to include all the ROS2 libraries
 . ${ROS2_ROOT}/cheri_setup.sh
-
-ldd ${ROS2_TEST}
 
 if "${ROS2_TEST}"
 then
