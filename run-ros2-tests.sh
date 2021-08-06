ecc384593187e163aa1201cabab9c96c860007af
Michael Dodson <md403@cam.ac.uk>
2021-08-06 23:14:08 +0100

    Update test script for new cheribuild install location

--- run-ros2-tests.sh
+++ run-ros2-tests.sh
@@ -3,20 +3,23 @@
 # test run via cheribuild:
 # ./cheribuild.py ros2-mips-purecap --test (or similar)
 
-ROS2_ROOT="$(realpath .)"
-if ! test -e "${ROS2_ROOT}/run-ros2-tests.sh"; then
+SOURCE="$(realpath .)"
+INSTALL="/opt/riscv64-purecap"
+
+if ! test -e "${SOURCE}/run-ros2-tests.sh"; then
    echo "You have to cd to the directory where $0 is located first!"
    exit 1
 fi
 
-ROS2_TEST="${ROS2_ROOT}/install/ros2_cheribsd_test/lib/ros2_cheribsd_test/cheribsd_test"
+# Can cheribuild set a host environment variable so we know the prefix for this test?
+TEST="${INSTALL}/ros2/lib/ros2_cheribsd_test/cheribsd_test"
 
 # set LD_CHERI_LIBRARY_PATH and LD_LIBRARY_PATH to include all the ROS2 libraries
-. ${ROS2_ROOT}/cheri_setup.sh
+. ${INSTALL}/ros2/cheri_setup.sh
 
-if "${ROS2_TEST}"
+if "${TEST}"
 then
-	echo "TESTS SUCCCEEDED"
+	echo "TEST SUCCEEDED"
 else
     echo "Got test failures"
     false
